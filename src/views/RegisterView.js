8c729e77e6570724d393b9485a409655bc0f5410
Dima Savkov <dimsaw85@gmail.com>
2022-08-06 22:20:10 +0300

    ready register

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,22 +1,70 @@
-function RegisterView() {
+import { useState } from 'react';
+import { useSignUpMutation } from 'redux/contactsApi';
+
+export default function RegistredView() {
+  const [params, setParams] = useState({ name: '', email: '', password: '' });
+  const [signUp, { isLoading }] = useSignUpMutation();
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    setParams({ ...params, [name]: value });
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await signUp(params);
+    if (response?.data?.token) {
+      console.log(response?.data?.token);
+      console.log(`Sign up success`);
+      reset();
+    } else {
+      console.log(`Check your info and try again`);
+      setParams({ ...params, password: '' });
+    }
+  }
+
+  const reset = () => {
+    setParams({ name: '', email: '', password: '' });
+  };
   return (
-    <h4>
-      Serif fonts refer to fonts that have small decorative flourishes at the
-      end of strokes. In some situations, serif fonts are also referred to as
-      “Roman”. The most common serif font is Times New Roman. It is said that
-      serifs make the individual letters more distinctive and our brain can
-      spend less time on recognizing letters so Serif fonts are considered to be
-      easier to read on printed material such as books and newspapers, where
-      they are used most. However, things may be different for the web. Since
-      computer screen has a much lower resolution than printed works like books
-      and newspapers, serif fonts can make the letters pixilated and hard to
-      read. Hence, for online publishing, sans serif fonts are usually
-      preferred. Tabac designed by Suitcase Type Foundry is a typical serif font
-      family. It is a static typeface with modern shapes and distinct, wedge
-      shaped serifs and it is mainly designed for the setting of newspapers,
-      magazines and books.
-    </h4>
+    <form onSubmit={handleSubmit}>
+      <h2>Log in your account</h2>
+      <label>
+        Name
+        <input
+          type="name"
+          name="name"
+          value={params.name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="email"
+          name="email"
+          value={params.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={params.password}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button variant="success" type="submit">
+        register
+      </button>
+    </form>
   );
 }
-
-export default RegisterView;
