53d62c8e41cd2c6f13b76bf028a978314c0fc4b9
Dima Savkov <dimsaw85@gmail.com>
2022-08-13 19:24:34 +0300

    App ready

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 
 import { getToken } from './redux/contact_selector';
 import { useGetCurrentUserMutation } from './redux/contactsApi';
+import {
+  PublicRoute,
+  PrivateRoute,
+  CustomerRoute,
+} from './componets/CustomerRoute/CustomerRoute.js';
 // import Container from './componets/Container';
 import HomeView from './views/HomeView';
 import AppBar from './componets/AppBar/AppBar';
 import RegisterView from './views/RegisterView';
-import NotFoundPage from './views/NotFoundPage';
+// import NotFoundPage from './views/NotFoundPage';
 import LoginView from './views/LoginView';
 import PhonebookView from './views/PhonebookView';
-import LogoutView from './views/LogoutView';
+// import LogoutView from './views/LogoutView';
 
 export default function App() {
   const [content, setContent] = useState(false);
@@ -36,12 +41,41 @@ export default function App() {
         <AppBar />
         <Suspense fallback={'Loading.....'}>
           <Routes>
-            <Route path="/" element={<HomeView />} />
-            <Route path="contatcs" element={<PhonebookView />} />
-            <Route path="register" element={<RegisterView />} />
-            <Route path="login" element={<LoginView />} />
-            <Route path="logout" element={<LogoutView />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <HomeView />
+                </PublicRoute>
+              }
+            />
+
+            <Route
+              path="register"
+              element={
+                <PublicRoute restricted>
+                  <RegisterView />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute restricted>
+                  <LoginView />
+                </PublicRoute>
+              }
+            />
+
+            <Route
+              path="contatcs"
+              element={
+                <PrivateRoute restricted>
+                  <PhonebookView />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<CustomerRoute restricted />} />
           </Routes>
         </Suspense>
       </>
