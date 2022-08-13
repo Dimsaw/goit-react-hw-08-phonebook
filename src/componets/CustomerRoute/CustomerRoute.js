fea3801d969f48e74b30b3538d9b0baa818521a9
Dima Savkov <dimsaw85@gmail.com>
2022-08-13 19:10:45 +0300

    ready routes

--- src/componets/CustomerRoute/CustomerRoute.js
+++ src/componets/CustomerRoute/CustomerRoute.js
@@ -6,3 +6,14 @@ export function PrivateRoute({ children }) {
   const isLoggedIn = useSelector(getIsLoggedIn);
   return <> {isLoggedIn ? <Navigate to="/" /> : <Navigate to={'login'} />}</>;
 }
+
+export function PublicRoute({ children, restricted = false }) {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  const shouldRedirect = isLoggedIn && restricted;
+  return <>{shouldRedirect ? <Navigate to="/" /> : children}</>;
+}
+
+export function CustomerRoute() {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  return <> {isLoggedIn ? <Navigate to="/" /> : <Navigate to={'login'} />}</>;
+}
