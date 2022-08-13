8ede6e7049af305815115bc6a6975fecbfc92407
Dima Savkov <dimsaw85@gmail.com>
2022-08-13 19:36:58 +0300

    userMenu

--- src/componets/UserMenu/UserMenu.js
+++ src/componets/UserMenu/UserMenu.js
@@ -1,8 +1,30 @@
-// export default function UserMenu() {
-//   return (
-//     <div>
-//       <span>Hello!</span>
-//       <button type="button"></button>
-//     </div>
-//   );
-// }
+import { useLogOutMutation } from 'redux/contactsApi';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUsername } from 'redux/contact_selector';
+import { changeFilter } from 'redux/actions';
+
+export default function UserMenu() {
+  const name = useSelector(getUsername);
+  const dispatch = useDispatch();
+  const [logOut, { isLoading }] = useLogOutMutation;
+
+  function exit() {
+    dispatch(changeFilter(''));
+    logOut();
+  }
+
+  return (
+    <div>
+      <span>Hello, {name}</span>
+      <button type="button" onClick={() => exit()} disabled={isLoading}>
+        {isLoading ? 'Log outing...' : 'Log out'} {''}
+        <img
+          src="https://img.icons8.com/ios-filled/344/emergency-exit.png"
+          width="20"
+          height="20"
+          alt="exit"
+        />
+      </button>
+    </div>
+  );
+}
